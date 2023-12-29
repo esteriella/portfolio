4a5ad2280249c090cd510ba0f5fbdeae2c55356e
Opeyemi Esther Agbaje <damamy96@gmail.com>
2023-12-29 21:10:21 +0100

    Revert "Merge branch 'master' of https://github.com/esteriella/portfolio"
    This reverts commit 8d26f499e0395a46c069fbf9fb32eb5f8b348543, reversing
    changes made to 6ea62f637229a3646fb10c3ddf6290f55c19f2de.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  // experimental: {
-  //   serverActions: true,
-  // },
-};
+    // experimental: {
+    //     serverActions: true,
+    // },
+    env:{
+        RESEND_API_KEY: process.env.RESEND_API_KEY
+    }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
